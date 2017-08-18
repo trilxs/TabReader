836c09d1e6abe9c93b24afa5ad5a033a39739fca
trilxs <tamlumusic@gmail.com>
2017-08-17 22:25:25 -0700

    Added new activities

--- app/src/main/java/tamlumusic/tabit/Local.java
+++ app/src/main/java/tamlumusic/tabit/Local.java
@@ -1,5 +1,6 @@
 package tamlumusic.tabit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class Local extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local);
+    }
+
+    public void InformationButtonClick() {
+        Intent intent = new Intent(getApplicationContext(), VideoInformation.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/tamlumusic/tabit/VideoInformation.java
@@ -0,0 +1,13 @@
+package tamlumusic.tabit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_information);
+    }
+}
